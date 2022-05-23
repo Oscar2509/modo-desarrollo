30382b398491124e112774ae64ea8ee043bd40d6
Oscar2509 <oscar2509098@comunidad.unam.mx>
2022-05-23 03:37:06 -0500

    first commit

---
+++ frontend/vistas/modulos/footer.php
@@ -0,0 +1,180 @@
+<!--=====================================
+FOOTER
+======================================-->
+
+<footer class="container-fluid">
+
+	<div class="container">
+
+		<div class="row">
+
+		 	<!--=====================================
+			CATEGORÍAS Y SUBCATEGORÍAS FOOTER
+			======================================-->
+
+			<div class="col-lg-5 col-md-6 col-xs-12 footerCategorias">
+
+			<?php
+
+				$url = Ruta::ctrRuta();
+
+				$item = null;
+				$valor = null;
+
+				$categorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
+
+				foreach ($categorias as $key => $value) {
+
+					echo '<div class="col-lg-4 col-md-3 col-sm-4 col-xs-12">
+
+						<h4><a href="'.$url.$value["ruta"].'" class="pixelCategorias" titulo="'.$value["categoria"].'">'.$value["categoria"].'</a></h4>
+
+						<hr>
+
+						<ul>';
+
+						$item = "id_categoria";
+
+						$valor = $value["id"];
+
+						$subcategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
+						
+						foreach ($subcategorias as $key => $value) {
+						
+							echo '<li><a href="'.$url.$value["ruta"].'" class="pixelSubCategorias" titulo="'.$value["subcategoria"].'">'.$value["subcategoria"].'</a></li>';
+
+						}
+
+						echo '</ul>
+
+					</div>';
+
+				}
+
+			?>
+
+			</div>
+
+			<!--=====================================
+			DATOS CONTACTO
+			======================================-->
+
+			<div class="col-md-3 col-sm-6 col-xs-12 text-left infoContacto">
+				
+				<h5>Dudas e inquietudes, contáctenos en:</h5>
+
+				<br>
+				
+				<h5>
+					
+					<i class="fa fa-phone-square" aria-hidden="true"></i> (555) 555-55-55
+
+					<br><br>
+
+					<i class="fa fa-envelope" aria-hidden="true"></i> soporte@tiendavirtual.com
+
+					<br><br>
+
+					<i class="fa fa-map-marker" aria-hidden="true"></i> FES ACATLÁN
+
+					<br><br>
+					México | México
+
+				</h5>
+
+				<iframe src="https://goo.gl/maps/k9YCu5P98o7eKdAn8" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>	
+
+			</div>
+
+			<!--=====================================
+			FORMULARIO CONTÁCTENOS
+			======================================-->
+
+			<div class="col-lg-4 col-md-3 col-sm-6 col-xs-12 formContacto">
+				
+				<h4>RESUELVA SU INQUIETUD</h4>
+
+				<form role="form" method="post" onsubmit="return validarContactenos()">
+
+			  		<input type="text" id="nombreContactenos" name="nombreContactenos" class="form-control" placeholder="Escriba su nombre" required> 
+
+			   		<br>
+	    	      
+   					<input type="email" id="emailContactenos" name="emailContactenos" class="	form-control" placeholder="Escriba su correo electrónico" required>  
+
+   					<br>
+	    		     	          
+	       			<textarea id="mensajeContactenos" name="mensajeContactenos" class="form-control" placeholder="Escriba su mensaje" rows="5" required></textarea>
+
+	       			<br>
+	    	
+	       			<input type="submit" value="Enviar" class="btn btn-default backColor pull-right" id="enviar">         
+
+				</form>
+
+				<?php 
+
+					$contactenos = new ControladorUsuarios();
+					$contactenos -> ctrFormularioContactenos();
+
+				?>
+
+			</div>
+			
+		</div>
+
+	</div>
+
+</footer>
+
+
+<!--=====================================
+FINAL
+======================================-->
+
+<div class="container-fluid final">
+	
+	<div class="container">
+	
+		<div class="row">
+			
+			<div class="col-sm-6 col-xs-12 text-left text-muted">
+				
+				<h5>&copy; 2017 Todos los derechos reservados. Sitio elaborado por equipo de Desarrollo Web</h5>
+
+			</div>
+
+			<div class="col-sm-6 col-xs-12 text-right social">
+				
+			<ul>
+
+			<?php
+				
+			$social = ControladorPlantilla::ctrEstiloPlantilla();
+
+				$jsonRedesSociales = json_decode($social["redesSociales"],true);		
+
+				foreach ($jsonRedesSociales as $key => $value) {
+
+					if($value["activo"] == 1){
+
+						echo '<li>
+								<a href="'.$value["url"].'" target="_blank">
+									<i class="fa '.$value["red"].' redSocial '.$value["estilo"].'" aria-hidden="true"></i>
+								</a>
+							</li>';
+
+					}
+				}
+
+			?>
+
+			</ul>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
